7c34719e414f7893ea6ac4864d0ca202b65c22db
Olivier AKYOL <olivier.akyol@gmail.com>
2021-12-02 15:46:02 +0100

    Partie 2

---
+++ src/main/java/fr/lernejo/guessgame/HumanPlayer.java
@@ -0,0 +1,11 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+import java.util.Scanner;
+
+public class HumanPlayer implements Player {
+
+    private final Logger logger = LoggerFactory.getLogger("Player");
+
+}
---
+++ src/main/java/fr/lernejo/logger/Logger.java
@@ -0,0 +1,10 @@
+package fr.lernejo.logger;
+
+public interface Logger {
+
+    void log(String message);
+
+
+
+
+}
